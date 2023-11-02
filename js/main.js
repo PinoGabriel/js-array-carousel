6e22a8bfb3d94a2b622e5d15cf89044a77e5ed08
PinoGabriel <pinogabriel01@gmail.com>
2023-11-02 16:50:25 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+let images = document.querySelectorAll("img")
+console.log(images.length);
+let btnUp = document.getElementById("btnUp")
+let currentImg = 0
+images[currentImg];
+console.log(images[currentImg]);
+
+btnUp.addEventListener("click", function() {
+    console.log("avanti");
+    currentImg+1;
+    console.log(currentImg+1)
+
+    if (currentImg < images.length - 1) {
+        images[currentImg].classList.remove("my_display");
+        currentImg++;
+        images[currentImg].classList.add("my_display");
+        console.log(images[currentImg])
+    }
+    
+})
